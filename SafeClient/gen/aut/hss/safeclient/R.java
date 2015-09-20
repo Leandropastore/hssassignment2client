dae2a3ef013a7b0c7d46948fbb24d9d9c53df121
Jared Kwok <jaredtkwok@hotmail.com>
2015-09-20 18:01:10 +1200

    Updated Interface

--- SafeClient/gen/aut/hss/safeclient/R.java
+++ SafeClient/gen/aut/hss/safeclient/R.java
@@ -25,7 +25,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int CancelButton=0x7f080005;
+        public static final int LoginButton=0x7f080004;
+        public static final int Password=0x7f080003;
+        public static final int TextBox=0x7f080001;
+        public static final int Username=0x7f080002;
+        public static final int action_settings=0x7f080006;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,9 +40,14 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Cancel_fix=0x7f050006;
+        public static final int Encrpyted_Message_Service=0x7f050003;
+        public static final int Login_button=0x7f050004;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int cancel_fix=0x7f050007;
         public static final int hello_world=0x7f050001;
+        public static final int login_fix=0x7f050005;
     }
     public static final class style {
         /** 
